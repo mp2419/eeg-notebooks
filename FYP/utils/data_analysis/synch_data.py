d639ee3ee611e01534fa1977d31dc681d44b726c
mp2419 <59872205+mp2419@users.noreply.github.com>
2023-03-09 17:40:01 +0000

    synch

--- FYP/utils/data_analysis/synch_data.py
+++ FYP/utils/data_analysis/synch_data.py
@@ -1,19 +1,19 @@
-#TODO: 
-
 # - read two csv
 # - get timestamp of marker and see when next timestamp of recording is bigger that marker, add marker in recording and iterate for each marker
 # - new csv with sync data between marker and recording data
+
 import csv
 import numpy as np
 
 def merge_data(filename_raw, filename_marked, filename_union):
+    
     # Open the two CSV files and the output CSV file
     with open(filename_raw, 'r') as raw, open(filename_marked, 'r') as markers, open(filename_union, 'w', newline='') as merged:
         # Create CSV readers for both files and a writer for the output file
         # reader1 = np.genfromtxt(raw, delimiter=',', skip_header=1)
-        # print(reader1)
         csv_reader1 = csv.reader(raw)
         data = [row for row in csv_reader1]
+
         data_rows = []
         for row in data[1:]:
             n0 = float(row[0])
@@ -23,8 +23,9 @@ def merge_data(filename_raw, filename_marked, filename_union):
             n4 = float(row[4])
             data_rows.append((n0,n1,n2,n3,n4))
         reader1 = np.array(data_rows)
-        print(type(reader1))
+
         #----------
+
         csv_reader2 = csv.reader(markers)
         data = [row for row in csv_reader2]
         data_tuples = []
@@ -34,29 +35,29 @@ def merge_data(filename_raw, filename_marked, filename_union):
             data_tuples.append((float_val, string_val))
         reader2 = np.array(data_tuples)
 
-        print(type(reader2))
+        #----------
+
         writer = csv.writer(merged)
         writer.writerow(['Timestamp','TP9','AF7','AF8','TP10','Marker', 'Marker_timestamp'])
         idx1 = 0
-        true = reader2[idx1][0]> reader1[0][0]
-
+        row_newfile = []
+        done = False
+        rows = reader2[idx1]
+        print(len(reader2))
         for i in reader1:
-            #print("timestamp marker", reader2[idx1][0]  ,"timestamp recording",  rows[0])
-            print()
-            rows = reader2[idx1]
-            if rows[0] <= i[0]:
-                row_newfile = [rows[0],rows[1],rows[2],rows[3],rows[4],reader1[idx1][1], reader2[idx1][0] ]
-                idx1 =+ 1
+            if (float(rows[0]) < float(i[0])) & (done == False):
+                row_newfile = [i[0],i[1],i[2],i[3],i[4],rows[1], rows[0]] 
+                print('new event: ', idx1)
+                if idx1 == len(reader2)-1:
+                    print('all events recorderd')
+                    done = True
+                else:
+                    idx1 = idx1+1
+                    rows = reader2[idx1]
             else:
-                row_newfile = [rows[0],rows[1],rows[2],rows[3],rows[4],'', '']
+                row_newfile = [i[0],i[1],i[2],i[3],i[4],'', '']
             
             writer.writerow(row_newfile)
-
-            if idx1 == len(reader2):
-                break
-        
-        if idx1 < len(reader2):
-            print("need to add last makeres!!")
         
     return filename_union
 
