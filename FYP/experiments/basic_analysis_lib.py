f2d5a40a6cb5bb3f4dcb6c16becd76e73974d43d
mp2419 <59872205+mp2419@users.noreply.github.com>
2023-06-01 20:15:27 +0100

    all performance TODO

--- FYP/experiments/basic_analysis_lib.py
+++ FYP/experiments/basic_analysis_lib.py
@@ -1,47 +1,19 @@
 import copy
-from copy import deepcopy
-import math
-import logging
-from collections import OrderedDict
-from glob import glob
-from typing import Union, List, Dict
-from collections import Iterable
-from time import sleep, time
-from numpy.core.fromnumeric import std
-import keyboard
-import os
+from typing import List
+from time import time
+import os, csv, json
 import time
-import eegnb
-from eegnb.devices.eeg import EEG
 import mne
-import threading
-import csv
-import random
-from queue import Queue
-import muselsl
-from muselsl import stream
-import argparse
 import numpy as np  # Module that simplifies computations on matrices
 import matplotlib.pyplot as plt  # Module used for plotting
 from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
-from psychopy import prefs
 from graphics import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from mne import create_info, concatenate_raws
 from mne.io import RawArray
 from mne.channels import make_standard_montage
-from mne.filter import create_filter
-from matplotlib import pyplot as plt
-from scipy.signal import lfilter, lfilter_zi
-
-from eegnb.devices.eeg import EEG
-from eegnb.devices.utils import EEG_INDICES, SAMPLE_FREQS
-import os
-import warnings
-warnings.filterwarnings('ignore')
 
 
 def load_csv_as_raw(
@@ -213,3 +185,131 @@ def psd_eeg(file_path, n=4):
     plt.ylabel('Power Spectral Density')
     plt.legend()
     plt.show()
+
+#----------------------------------------
+
+def extract_arrow_stats(json_file_path):
+    with open(json_file_path, 'r') as file:
+        eeg_data = json.load(file)
+        eeg_data = np.array(eeg_data)
+        marker_strings = eeg_data[:, 5]
+        marker_timestamp = eeg_data[:, 0]
+
+        left_arrow_delay = []
+        right_arrow_delay = []
+        stats = {'Dir_Missed_pcg':0, 'Dir_Wrong_pcg':0, 'Dir_empty_call':0, 'L_correct': 0, 'L_missed': 0, 'L_wrong': 0, 'R_correct': 0, 'R_missed': 0, 'R_wrong': 0, 'L_delay_avg': 0, 'R_delay_avg':0, 'L_delay_std': 0, 'R_delay_std':0}
+        prev_left = False
+        prev_right = False
+
+        for i in range(len(marker_strings)):
+            value = marker_strings[i]
+           
+            timestamp = float(marker_timestamp[i])
+
+            if value == "left arrow":
+                if prev_left:
+                    left_arrow_delay.append(timestamp - prev_left_timestamp)
+                    prev_left = False
+                elif prev_right:
+                    stats['R_wrong'] += 1
+                else:
+                    stats['Dir_empty_call'] += 1
+
+            elif value == "right arrow":
+                if prev_right:
+                    right_arrow_delay.append(timestamp - prev_right_timestamp)
+                    prev_right = False
+                elif prev_left:
+                    stats['L_wrong'] += 1
+                else:
+                    stats['Dir_empty_call'] += 1
+
+            elif value == "left":
+                if prev_left:
+                    stats['L_missed'] += 1
+                if prev_right:
+                    stats['R_missed'] += 1
+                
+                prev_left = True
+                prev_right = False
+                prev_left_timestamp = timestamp
+
+            elif value == "right":
+                if prev_left:
+                    stats['L_missed'] += 1
+                if prev_right:
+                    stats['R_missed'] += 1
+                
+                prev_right = True
+                prev_left = False
+                prev_right_timestamp = timestamp
+
+        if prev_left:
+            stats['L_missed'] += 1
+        if prev_right:
+            stats['R_missed'] += 1
+
+        stats['L_correct'] = len(left_arrow_delay)
+        stats['R_correct'] = len(right_arrow_delay)
+        tot_correct = stats['L_correct'] + stats['R_correct']
+
+        stats['L_delay_avg'] = np.mean(left_arrow_delay)
+        stats['R_delay_avg'] = np.mean(right_arrow_delay)
+
+        stats['L_delay_std'] = np.std(left_arrow_delay)
+        stats['R_delay_std'] = np.std(right_arrow_delay)
+
+        stats['Dir_Missed_pcg'] = (stats['L_missed'] + stats['R_missed']) / tot_correct *100
+        stats['Dir_Wrong_pcg'] = (stats['L_wrong'] + stats['R_wrong']) / tot_correct *100
+
+    return left_arrow_delay, right_arrow_delay, stats
+
+#-------------
+
+# json_file_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data_ordered', 'data_json', 'AudioVisual_04_2.json')
+# left_arrow_delay, right_arrow_delay, stats = extract_arrow_stats(json_file_path)
+# print(stats)
+
+#----------------------------
+
+def merge_obj_performance(circles_file_path, eeg_json_path):
+
+    with open(circles_file_path, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader) #leave
+        first_row = next(reader)
+        true = float(first_row[0])
+        reported = float(first_row[1])
+        error = reported-true
+        error_pcg = error/true *100
+
+        left_arrow_delay, right_arrow_delay, stats = extract_arrow_stats(eeg_json_path)
+
+        performance = {}
+        performance['Cir_Error_pcg'] = error_pcg
+        performance['Cir_Error'] = error
+        for key in stats.keys():
+            performance[key] = stats[key]
+        performance['L_delay_array'] = left_arrow_delay
+        performance['R_delay_array'] = right_arrow_delay
+
+
+    
+    return performance
+
+#--------------
+
+# circles_file_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data', 'AudioVisual', '04', '2', 'blue_circles.csv')
+# eeg_json_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data_ordered', 'data_json', 'AudioVisual_04_2.json')
+# new_file_json = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'results_data', 'Arrow_Circles_performance.json')
+
+
+# performance = merge_obj_performance(circles_file_path, eeg_json_path)
+# all_performances = {}
+# all_performances['AudioVisual_04_2'] = performance
+
+# json_data = json.dumps(all_performances, indent=4)
+
+# with open(new_file_json, 'w') as file:
+#     file.write(json_data)
+#     print("New file at ", new_file_json)
--- FYP/experiments/utils/arrow_stats.py
+++ FYP/experiments/utils/arrow_stats.py
@@ -10,7 +10,7 @@ def extract_arrow_stats(json_file_path):
 
         left_arrow_delay = []
         right_arrow_delay = []
-        stats = {'L_correct': 0, 'L_missed': 0, 'L_wrong': 0, 'R_correct': 0, 'R_missed': 0, 'R_wrong': 0, 'empty_call':0, 'L_delay_avg': 0, 'R_delay_avg':0, 'L_delay_std': 0, 'R_delay_std':0}
+        stats = {'Dir_Missed_pcg':0, 'Dir_Wrong_pcg':0, 'Dir_empty_call':0, 'L_correct': 0, 'L_missed': 0, 'L_wrong': 0, 'R_correct': 0, 'R_missed': 0, 'R_wrong': 0, 'L_delay_avg': 0, 'R_delay_avg':0, 'L_delay_std': 0, 'R_delay_std':0}
         prev_left = False
         prev_right = False
 
@@ -26,7 +26,7 @@ def extract_arrow_stats(json_file_path):
                 elif prev_right:
                     stats['R_wrong'] += 1
                 else:
-                    stats['empty_call'] += 1
+                    stats['Dir_empty_call'] += 1
 
             elif value == "right arrow":
                 if prev_right:
@@ -35,7 +35,7 @@ def extract_arrow_stats(json_file_path):
                 elif prev_left:
                     stats['L_wrong'] += 1
                 else:
-                    stats['empty_call'] += 1
+                    stats['Dir_empty_call'] += 1
 
             elif value == "left":
                 if prev_left:
@@ -64,10 +64,16 @@ def extract_arrow_stats(json_file_path):
 
         stats['L_correct'] = len(left_arrow_delay)
         stats['R_correct'] = len(right_arrow_delay)
+        tot_correct = stats['L_correct'] + stats['R_correct']
+
         stats['L_delay_avg'] = np.mean(left_arrow_delay)
         stats['R_delay_avg'] = np.mean(right_arrow_delay)
+
         stats['L_delay_std'] = np.std(left_arrow_delay)
         stats['R_delay_std'] = np.std(right_arrow_delay)
+
+        stats['Dir_Missed_pcg'] = (stats['L_missed'] + stats['R_missed']) / tot_correct *100
+        stats['Dir_Wrong_pcg'] = (stats['L_wrong'] + stats['R_wrong']) / tot_correct *100
 
     return left_arrow_delay, right_arrow_delay, stats
 
@@ -93,27 +99,30 @@ def merge_obj_performance(circles_file_path, eeg_json_path):
         left_arrow_delay, right_arrow_delay, stats = extract_arrow_stats(eeg_json_path)
 
         performance = {}
-        performance = stats
+        performance['Cir_Error_pcg'] = error_pcg
+        performance['Cir_Error'] = error
+        for key in stats.keys():
+            performance[key] = stats[key]
         performance['L_delay_array'] = left_arrow_delay
         performance['R_delay_array'] = right_arrow_delay
-        performance['circle_error'] = error
-        performance['circle_error_pcg'] = error_pcg
+
+
     
     return performance
 
 #--------------
 
-circles_file_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data', 'AudioVisual', '04', '2', 'blue_circles.csv')
-eeg_json_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data_ordered', 'data_json', 'AudioVisual_04_2.json')
-new_file_json = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'results_data', 'Arrow_Circles_performance.json')
+# circles_file_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data', 'AudioVisual', '04', '2', 'blue_circles.csv')
+# eeg_json_path = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'data_ordered', 'data_json', 'AudioVisual_04_2.json')
+# new_file_json = os.path.join(os.path.expanduser('~/'), 'Desktop', 'FYP', 'code_env', 'eeg-notebooks', 'FYP', 'results_data', 'Arrow_Circles_performance.json')
 
 
-performance = merge_obj_performance(circles_file_path, eeg_json_path)
-all_performances = {}
-all_performances['AudioVisual_04_2'] = performance
+# performance = merge_obj_performance(circles_file_path, eeg_json_path)
+# all_performances = {}
+# all_performances['AudioVisual_04_2'] = performance
 
-json_data = json.dumps(all_performances, indent=4)
+# json_data = json.dumps(all_performances, indent=4)
 
-with open(new_file_json, 'w') as file:
-    file.write(json_data)
-    print("New file at ", new_file_json)
+# with open(new_file_json, 'w') as file:
+#     file.write(json_data)
+#     print("New file at ", new_file_json)
